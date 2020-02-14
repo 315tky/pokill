c7aa43ccd4c1a9c2e40e9a81c9df33b2e9bf3dcc
315tky <315tky@gmail.com>
2020-02-14 12:28:09 +0900

    ActiveRecord Import for Killmail imports

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ ruby '2.6.5'
 gem 'swagger_client', :git => 'https://github.com/315tky/eve-esi-api-swagger-gem.git'
 
 gem 'mysql2'
+gem 'activerecord-import'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.2', '>= 6.0.2.1'
--- lib/esi-get-recent-corp-kills.rb
+++ lib/esi-get-recent-corp-kills.rb
@@ -19,11 +19,9 @@ require 'pp'
       "grant_type" => "refresh_token",
       "refresh_token" => "#{ENV['REFRESH_TOKEN']}"
     })
-
     req_options = {
       use_ssl: uri.scheme == "https",
     }
-
     response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
       http.request(request)
     end
@@ -34,7 +32,6 @@ require 'pp'
       config.access_token = session_access_token
     end
     @api_instance = SwaggerClient::KillmailsApi.new
-pp "get auth ran"
   end
 
   def get_corp_killmails_meta(corporation_id)
@@ -47,7 +44,6 @@ pp "get auth ran"
     }
     begin
       all_meta_killmails = @api_instance.get_corporations_corporation_id_killmails_recent(corporation_id, all_killmails_opts)
-pp "got all meta killmails"
     rescue SwaggerClient::ApiError => e
       @logger.error "Exception when calling KillmailsApi->get_corporations_corporation_id_killmails_recent: #{e}"
     end
@@ -55,6 +51,7 @@ pp "got all meta killmails"
   end
 
   def check_db(all_meta_killmails) # check pokill db for killmail already imported, return any that are not
+
     for_import = []
     all_meta_killmails.each do |meta_killmail|
        killmail_id = meta_killmail.killmail_id
@@ -70,7 +67,7 @@ pp "got all meta killmails"
   end
 
   def get_killmail_details(for_import)
-pp "starting to look up single mails from meta"
+
     single_killmail_opts = {
       datasource: 'tranquility', # String | The server name you would like data from
       if_none_match: 'if_none_match_example', # String | ETag from a previous request. A 304 will be returned if this matches the current ETag
@@ -91,53 +88,52 @@ pp "starting to look up single mails from meta"
   end
 
   def import_killmail_details(single_killmails)
+pp "starting imports"
     ActiveRecord::Base.transaction do
+      for_import = []
       single_killmails.each do |killmail|
-        killmail_details = {  "killmail_id"     => killmail.killmail_id ||= '',
-                              "killmail_time"   => killmail.killmail_time ||= '',
-                              "solar_system_id" => killmail.solar_system_id ||= '',
-                              "victim_id"       => killmail.victim.character_id ||= '',
-                              "victim_corporation_id" => killmail.victim.corporation_id ||= '',
-                              "victim_damage_taken"   => killmail.victim.damage_taken ||= '',
-  			                      "victim_position"       => killmail.victim.position.to_json ||= '',
-                              "victim_ship_id"        => killmail.victim.ship_type_id ||= '' }
-        km = Killmail.create(killmail_details)
-        km.save
+         kill_hash = {  "killmail_id"           => killmail.killmail_id ||= '',
+                        "killmail_time"         => killmail.killmail_time ||= '',
+                        "solar_system_id"       => killmail.solar_system_id ||= '',
+                        "victim_id"             => killmail.victim.character_id ||= '',
+                        "victim_corporation_id" => killmail.victim.corporation_id ||= '',
+                        "victim_damage_taken"   => killmail.victim.damage_taken ||= '',
+                        "victim_position"       => killmail.victim.position.to_json ||= [],
+                        "victim_ship_id"        => killmail.victim.ship_type_id ||= ''
+                      }
 
+        for_import.push(kill_hash)
+
+        for_attackers_import = []
         killmail.attackers.each do |attacker|
-          killmail_attackers = killmail
-                               .attackers
-                               .map { |attacker| { "killmail_id"     => killmail.killmail_id ||= '',
-                                                   "attacker_id"     => attacker.character_id ||= '',
-                                                   "corporation_id"  => attacker.corporation_id ||= '',
-                                                   "alliance_id"     => attacker.alliance_id ||= '',
-                                                   "damage_done"     => attacker.damage_done ||= '',
-                                                   "final_blow"      => attacker.final_blow ||= false,
-                                                   "security_status" => attacker.security_status ||= '',
-                                                   "weapon_type_id"  => attacker.weapon_type_id ||= ''
-                                                   } }
-          killmail_attackers.each do |each_attacker|
-            km.killmail_attackers.create(each_attacker)
-          end
+
+          attackers_hash = { "killmail_id"     => killmail.killmail_id ||= '',
+                             "attacker_id"     => attacker.character_id ||= '',
+                             "corporation_id"  => attacker.corporation_id ||= '',
+                             "alliance_id"     => attacker.alliance_id ||= '',
+                             "damage_done"     => attacker.damage_done ||= '',
+                             "final_blow"      => attacker.final_blow ||= false,
+                             "security_status" => attacker.security_status ||= '',
+                             "weapon_type_id"  => attacker.weapon_type_id ||= ''
+                           }
+          for_attackers_import.push(attackers_hash)
         end
-        killmail_items = killmail
-                         .victim
-                         .items
-                         .map { |killmail_item| {  "killmail_id"        => killmail.killmail_id ||= '',
-                                                   "item_type_id"       => killmail_item.item_type_id ||= '',
-                                                   "flag_id"            => killmail_item.flag ||= '',
-                                                   "quantity_destroyed" => killmail_item.quantity_destroyed ||= '',
-                                                   "quantity_dropped"   => killmail_item.quantity_dropped ||= '',
-                                                   "singleton"          => killmail_item.singleton || ''
-                                                 } }
-        killmail_items.each do |item|
-          km.killmail_items.create(item)
+        KillmailAttacker.import for_attackers_import, on_duplicate_key_ignore: true # ActiveRecord Import
+
+        for_items_import = []
+        killmail.victim.items.each do |item|
+          items_hash =  {  "killmail_id"        => killmail.killmail_id ||= '',
+                           "item_type_id"       => item.item_type_id ||= '',
+                           "flag_id"            => item.flag ||= '',
+                           "quantity_destroyed" => item.quantity_destroyed ||= '',
+                           "quantity_dropped"   => item.quantity_dropped ||= '',
+                           "singleton"          => item.singleton || ''
+                         }
+          for_items_import.push(items_hash)
         end
+        KillmailItem.import for_items_import, on_duplicate_key_ignore: true # Active Record Import
       end
+      Killmail.import for_import # ActiveRecord Import
     end
   end
-
 end
-  # Add some code to this so that it checks each killmail_hash against the DB, or, do a check with ID, if can gurantee they are sequential. Then only looks up those in CCP ESI that are not found in local DB, less lookups to CCP ESI.
-  # Get last killmail_id from DB, then look up any that are higher than that number in CCP ESI.
-  # turn this into a class with 3 methods, then make a rake task that calls it, and gets the killmails.   # The rake task can update the database, need to check DB for last killmail timestamp and do a date relevant update for just those killmails not yet imported.
